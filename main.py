a060f3b905a8934862c71680a95583ddcff63687
Jim Doyle <indellibleink@gmail.com>
2025-05-21 07:31:30 -0400

    created simple add function

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-# Calulator
+## Calculator
+##----------------
+## Uncomment here the LCD Display
+## and resources/init.py
+## To enable Display
+
+# Imports
+
+
+# Funxtions
+def add():
+    num1 = input("num1:  ")
+    num2 = input("num2:  ")
+    return num1 + num2
+
+
+def main():
+
+    # Initalize LCD Display
+    # lcd = resources.LCD(2, 0x27, True)
+
+    # User Input
+    menu = input("calc:\n1) Add  ")
+
+    if menu == "1":
+        print(add())
+    elif menu == "2":
+        print("nothing")
+    elif menu == "3":
+        print("nothing")
+    else:
+        print("invalid input")
+    # Use Try / Catch while using file usage.
+
+    # Output to LCD Display
+    #    lcd.message(" record recorded")
+    #    time.sleep(10)
+    #    lcd.clear()
+
+    # End of Line
+    print("end of line")
+
+
+# Main
+if __name__ == "__main__":
+    main()
