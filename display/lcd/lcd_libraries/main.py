0573272e77ba9b30f3ce60e22e54fed9b333625f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2025-06-01 11:52:06 -0400

    added more display code

---
+++ display/lcd/lcd_libraries/main.py
@@ -0,0 +1,12 @@
+from pico_i2c_lcd import I2cLcd
+from machine import I2C
+from machine import Pin
+import utime as time
+ 
+ 
+i2c = I2C(id=1,scl=Pin(9),sda=Pin(8),freq=100000)
+lcd = I2cLcd(i2c, 0x27, 2, 16)
+ 
+while True:
+      lcd.move_to(2,0)
+      lcd.putstr('Hello world')
